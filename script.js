d19ed9d280cb120114e1a0249a4c5bd9d288ed98
Nikolai Kirnosoff <kalun62@gmail.com>
2022-01-31 22:25:56 +0300

    lesson03

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ for(let i = 1; i < num.length; i++){
 num = multyply ** 3;
 
 console.log(num.toString().substring(0, 2));
+
+//lesson 3-1
+
+let lang = 'en';
+
+let rusWeek = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+let engWeek = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
+let rusEngWeeks = [
+    ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+    ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
+];
+
+if(lang == 'ru'){
+    console.log(rusWeek);
+}else{
+    console.log(engWeek);
+}
+
+switch (lang){
+    case 'ru':
+        console.log(rusWeek);
+        break;
+    case 'en':
+        console.log(engWeek);
+        break;
+}
+
+lang == 'ru'? console.log(rusEngWeeks[0]) : console.log(rusEngWeeks[1]);
+
+//lesson 3-2
+
+let namePerson = "Артем";
+
+namePerson == "Артем"? console.log('Директор') : namePerson == "Александр"? console.log('преподаватель') : console.log('студент');
